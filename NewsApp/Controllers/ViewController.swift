a3e26b70fca8b682a3a86893f993325d4798ae20
atorresruiz8 <atorresruiz8@gmail.com>
2022-05-12 11:38:22 -0700

    Added search bar functionality to filter articles by a query.

--- NewsApp/Controllers/ViewController.swift
+++ NewsApp/Controllers/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
     
     private var articles = [Article]()
     private var viewModels = [NewsTableViewCellViewModel]()
+    private var searchVC = UISearchController(searchResultsController: nil)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +30,12 @@ class ViewController: UIViewController {
         view.backgroundColor = .systemBackground
         
         fetchTopStories()
+        createSearchBar()
+    }
+    
+    func createSearchBar() {
+        navigationItem.searchController = searchVC
+        searchVC.searchBar.delegate = self
     }
     
     func fetchTopStories() {
@@ -58,7 +65,7 @@ class ViewController: UIViewController {
     }
 }
 
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
+extension ViewController: UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return viewModels.count
     }
@@ -81,5 +88,31 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 150
+    }
+    
+    // Search
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let text = searchBar.text, !text.isEmpty else { return }
+        print(text)
+        
+        APICaller.shared.search(with: text, completion: {[weak self] result in
+            switch result {
+            case .success(let articles):
+                self?.articles = articles
+                self?.viewModels = articles.compactMap({
+                    NewsTableViewCellViewModel(title: $0.title,
+                                               subtitle: $0.description ?? "No Description",
+                                               imageURL: URL(string: $0.urlToImage ?? "")
+                    )
+                })
+                
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                    self?.searchVC.dismiss(animated: true)
+                }
+            case .failure(let error):
+                print(error)
+            }
+        })
     }
 }
--- NewsApp/Managers/APICaller.swift
+++ NewsApp/Managers/APICaller.swift
@@ -12,14 +12,33 @@ final class APICaller {
     
     struct Constants {
         static let topHeadlinesURL = URL(string: "https://newsapi.org/v2/top-headlines?country=US&apiKey=\(APIKeys.NEWS_API_KEY)")
+        static let searchURLString = "https://newsapi.org/v2/everything?apiKey=\(APIKeys.NEWS_API_KEY)&q="
+        //https://newsapi.org/v2/everything?apiKey=15d2509dcc7d490c883c2d5e4417c021&q=
     }
     
-    private init() {
-        
-    }
+    private init() {}
     
     public func getTopStories(completion: @escaping(Result<[Article], Error>) -> Void) {
         guard let url = Constants.topHeadlinesURL else { return }
+        let task = URLSession.shared.dataTask(with: url, completionHandler: { data, _, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let data = data {
+                do {
+                    let result = try JSONDecoder().decode(APIResponse.self, from: data)
+                    completion(.success(result.articles))
+                } catch {
+                    completion(.failure(error))
+                }
+            }
+        })
+        task.resume()
+    }
+    
+    public func search(with query: String, completion: @escaping(Result<[Article], Error>) -> Void) {
+        guard !query.trimmingCharacters(in: .whitespaces).isEmpty else { return }
+        let urlString = Constants.searchURLString + query
+        guard let url = URL(string: urlString) else { return }
         let task = URLSession.shared.dataTask(with: url, completionHandler: { data, _, error in
             if let error = error {
                 completion(.failure(error))
